dc727e9893aa5af082a3a67205e52644c18b9872
hamza elouham <hamzaelouham@gmail.com>
2022-05-14 18:34:56 +0100

    hamza

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
-# @Email:  contact@pythonandvba.com
-# @Website:  https://pythonandvba.com
-# @YouTube:  https://youtube.com/c/CodingIsFun
-# @Project:  Sales Dashboard w/ Streamlit
-
-
-
 import pandas as pd  # pip install pandas openpyxl
 import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
 
-# emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
 
 
+def getDataSets(arg):
+    return pd.read_excel(arg)
 
 
+
+data = getDataSets('datasets.xls')
+
+data["Month"] = data['Order Date'].dt.month
+
+chart_data = data.groupby(['Month']).sum()['Sales']
+
+st.write('This is a chart')
+st.bar_chart(chart_data)
 
 
 

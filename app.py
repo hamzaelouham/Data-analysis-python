6fbf7f41b3683a4444f448ea2dccbfa60221bae2
hamza elouham <hamzaelouham@gmail.com>
2022-05-15 17:04:48 +0100

    adding multiselect to shipmode

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd  # pip install pandas openpyxl
 import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
 
+
 st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
 
 
@@ -29,13 +30,15 @@ salesbymounth =  st.sidebar.multiselect(
     default=data["Month"].unique()
 )
 
-shipModeOptions = data['ShipMode'].unique()
+
 
 #filter by Shipping mode
-ship = st.sidebar.selectbox(
+ship = st.sidebar.multiselect(
     "select ship mode  :",
-    shipModeOptions
+    options=data["ShipMode"].unique(),
+    default=data["ShipMode"].unique()
 )
+
 
 #filter by category 
 category = st.sidebar.multiselect(
@@ -44,11 +47,48 @@ category = st.sidebar.multiselect(
     default=data["Category"].unique()
 )
 
+department = st.sidebar.multiselect(
+    "select Customer department :",
+    options=data["Department"].unique(),
+    default=data["Department"].unique()
+)
 
+# filter by region 
+region =  st.sidebar.multiselect(
+    "filter by  Customer region :",
+    options=data["Region"].unique(),
+    default=data["Region"].unique()
+)
+
+container = st.sidebar.multiselect(
+    "filter by  container :",
+    options=data["Container"].unique(),
+    default=data["Container"].unique()
+)
 
 
 sales =  data.query(
-    "Month == @salesbymounth & ShipMode == @ship & Category == @category"
+    "Month == @salesbymounth & ShipMode == @ship & Category == @category & Department == @department & Region == @region & Container == @container "
 )
+
+# styling dasboard
+st.title(":bar_chart: Sales Dashboard")
+st.markdown("##")
+
+total_sales = int(sales["Sales"].sum())
+total_profit = int(sales["Profit"].sum())
+
+left_column, right_column = st.columns(2)
+
+with left_column:
+    st.subheader("Total Sales:")
+    st.subheader(f"US $ {total_sales:,}")
+
+with right_column:
+    st.subheader("Tolal profit:")
+    st.subheader(f"US $ {total_profit}")
+
+st.markdown("""---""")
+
 
 st.dataframe(sales)
